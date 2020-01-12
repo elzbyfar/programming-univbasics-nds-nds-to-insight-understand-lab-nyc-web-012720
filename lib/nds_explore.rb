76872ad1aaa7a01a2e10cccd4e2a61b26cc82455
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-12 05:17:28 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,7 +12,7 @@ def print_first_directors_movie_titles(nds)
  while x < nds.length do 
    y = 0 
    while y < nds[x].length do 
-     puts nds[x][:movies][y]
+     puts nds[x][:movies][y].class
      y+=1 
    end 
    x+=1 
